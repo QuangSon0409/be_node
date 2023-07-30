1dc847eaba3cc1908e792e899e3c0328370fd58c
QuangSon0409 <buiquangson7782@gmail.com>
2023-07-31 00:10:00 +0700

    commit- import_update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import pokemonRouter from "./routes/pokemon";
+import pokemonRouter from "./routes/pokemon.js";
 import cors from "cors";
 const app = express();
 
@@ -15,5 +15,4 @@ app.use("/api", cors(corsOptions), pokemonRouter);
 app.listen(8080, () => {
   console.log(`App listen running at port : 8080`);
 });
-
 export const viteNodeApp = app;
--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAll, getPokemonById } from "../controllers/pokemon";
+import { getAll, getPokemonById } from "../controllers/pokemon.js";
 
 const router = express.Router();
 
